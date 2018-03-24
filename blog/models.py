93811ed3d981950c8b70f8bff900549fb33ca49e
Napabhat.M <napabht.m@gmail.com>
2018-03-24 14:17:53 +0700

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,7 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return '%s, %s' % (self.title, self.text)
+
+
 # Create your models here.
